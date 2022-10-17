1f323cde6e6e732d27703202dde5b43140d92270
Jelle Kaufmann <git@jellekaufmann.nl>
2022-10-17 19:16:53 +0200

    v2.2.4, fix influxdb debugging
    - fixed a typo introduced in v2.1.3 that broke sending raw A/D sensor data to InfluxDB
    - updated inline comments on InfluxDB's line protocol from v1.7 to more recent v2.4
    - added a screenshot with A/D readings from InfluxDB’s Data Explorer to README
    - v224

--- include/config.h
+++ include/config.h
@@ -77,7 +77,7 @@
 // to make Arduino IDE happy
 // version number is set in platformio.ini
 #ifndef FIRMWARE_VERSION
-#define FIRMWARE_VERSION 223
+#define FIRMWARE_VERSION 224
 #endif
 
 // set default port for MQTT over TLS
